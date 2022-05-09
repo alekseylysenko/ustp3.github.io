b400e450ddfad07eb1c93fd04f43d355386e62a7
alekseylysenko <lysenko-a@mail.ru>
2022-05-09 19:27:34 +0300

    3

--- src/components/LoadListInstructions.js
+++ src/components/LoadListInstructions.js
@@ -1,6 +1,6 @@
 
 import { useState , useEffect } from 'react';
-import {  useNavigate, Link} from 'react-router-dom';
+import { Link} from 'react-router-dom';
 import axios from 'axios';
 function LoadListInstructions(){
   
