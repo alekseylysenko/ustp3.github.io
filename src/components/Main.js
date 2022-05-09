2a3e5809b4db9a6b6366eec259a3df06816cf17f
alekseylysenko <lysenko-a@mail.ru>
2022-05-09 22:53:29 +0300

    3

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,13 +11,13 @@ function Main(){
     return(
         <HashRouter>
                        
-                    <Route path="ustp3.github.io" element={<Home />} />           
+                    <Route path="/ustp3.github.io" element={<Home />} />           
                     <Route path="/ustp3.github.io/admin" element={<Admin />} />
                     <Route path="/ustp3.github.io/firmwares" element={<Firmwares />} />    
                     <Route path="/ustp3.github.io/instructions" element={<Instructions />} />   
                     <Route path="/ustp3.github.io/instructions/:instructionId" element={<InstructionSinglePage />} />   
                     <Route path="/ustp3.github.io/burger-menu" element={<BurgerMenu />} />  
-                     
+                
               
         </HashRouter>  
        
