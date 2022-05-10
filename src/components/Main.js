a317f3b7f9c98641009fbbbb059b1f49349612da
alekseylysenko <lysenko-a@mail.ru>
2022-05-10 11:30:55 +0300

    4

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,15 +9,14 @@ import InstructionSinglePage from './InstructionSinglePage.js';
 
 function Main(){
     return(
-        <BrowserRouter  basename="/" >
+        <BrowserRouter  basename="/ustp3.github.io" >
                 <Routes>       
                     <Route path="/ustp3.github.io" element={<Home />} />           
                     <Route path="/ustp3.github.io/admin" element={<Admin />} />
                     <Route path="/ustp3.github.io/firmwares" element={<Firmwares />} />    
                     <Route path="/ustp3.github.io/instructions" element={<Instructions />} />   
                     <Route path="/ustp3.github.io/instructions/:instructionId" element={<InstructionSinglePage />} />   
-                    <Route path="/ustp3.github.io/burger-menu" element={<BurgerMenu />} />  
-                     
+                    <Route path="/ustp3.github.io/burger-menu" element={<BurgerMenu />} />                     
                 </Routes>
         </BrowserRouter>  
        
