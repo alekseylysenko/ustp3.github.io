e230dbb699c2c7609bc0722b21316b17b14fcb36
alekseylysenko <lysenko-a@mail.ru>
2022-05-10 11:17:37 +0300

    v

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,7 +11,7 @@ function Main(){
     return(
         <BrowserRouter>
                 <Routes>       
-                    <Route path="/ustp3.github.io" element={<Home />} />           
+                    <Route path="/" element={<Home />} />           
                     <Route path="/ustp3.github.io/admin" element={<Admin />} />
                     <Route path="/ustp3.github.io/firmwares" element={<Firmwares />} />    
                     <Route path="/ustp3.github.io/instructions" element={<Instructions />} />   
